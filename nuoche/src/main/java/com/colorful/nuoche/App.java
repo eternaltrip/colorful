781f64e0a7c030f16020d53c7de5502c420c196c
travel <travel@DESKTOP-LMN1HEI>
2020-06-09 17:31:24 +0800

    添加swagger-ui支持。

--- nuoche/src/main/java/com/colorful/nuoche/App.java
+++ nuoche/src/main/java/com/colorful/nuoche/App.java
@@ -6,26 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
 /**
  * Hello world!
  *
  */
 
-
-
 @SpringBootApplication
 @Controller
 public class App {
-	
+
 	public static void main(String[] args) {
-		//System.out.println("Hello World!");
+		// System.out.println("Hello World!");
 		SpringApplication.run(App.class, args);
 	}
-	
-	@RequestMapping("/")
-	@ResponseBody
-	public Object welcome() {
-		return "Hello ,Nuoche!";
-	}
-	
+
+
+
 }
